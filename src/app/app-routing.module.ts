afdf1fd5dcf813bed906ebac17091efd0e424703
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-06-10 15:24:22 +0100

    route to faces component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FacesComponent }      from './faces/faces.component';
+
+const routes: Routes = [
+	{ path: '', component: FacesComponent }
+];
 
 @NgModule({
+	imports: [ RouterModule.forRoot(routes) ],
   	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+	
+}
